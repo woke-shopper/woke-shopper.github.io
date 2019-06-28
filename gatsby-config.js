9d22df35226f8354dada9986aa8b808f4cb6dc41
Ayman Nadeem <aymannadeem@github.com>
2019-06-28 17:45:21 -0400

    more setup

---
+++ gatsby-config.js
@@ -0,0 +1,85 @@
+'use strict'
+
+module.exports = {
+  siteMetadata: {
+    title: 'WokeShopper',
+    description: 'Helping consumers navigate ethical choices.',
+    siteUrl: 'https://example.com',
+    author: {
+      name: 'Ayman Nadeem',
+      url: 'https://twitter.com/WokeShopper',
+      email: 'aymannadeem@gmail.com',
+    },
+    social: {
+      twitter: 'https://twitter.com/aymannadeem',
+      github: 'https://github.com/aymannadeem',
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/posts`,
+        name: `posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1280,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://wokeshopper.com`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Ayman Nadeem`,
+        short_name: `aymannadeem.com`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/logo.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-typescript`,
+    `gatsby-transformer-sharp`,
+  ],
+}
